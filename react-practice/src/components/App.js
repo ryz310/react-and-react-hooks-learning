bad2f9e429c532dd43e16cd253be80d6dc6a03c3
Ryosuke Sato <ryz310@gmail.com>
2021-11-21 21:18:34 +0900

    Modify the directory structure

---
+++ react-practice/src/components/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const todoDataUrl = "http://localhost:3100/todos";
+
+export const App = () => {
+  const [todoList, setTodoList] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const resposne = await axios.get(todoDataUrl);
+      setTodoList(response);
+    };
+    fetchData();
+  }, []);
+
+  console.log("TODO リスト", todoList);
+
+  return (
+    <>
+      <h1>TODO 進捗管理</h1>
+      <textarea />
+      <button>+ TODO を追加</button>
+
+      <h2>TODO リスト</h2>
+
+      <ul>
+        {todoList.map((todo) => {
+          <li key={todo.id} >
+            {todo.content}({todo.done ? "完了" : "未完了"});
+          </li>
+        })}
+      </ul>
+    </>
+  );
+}
